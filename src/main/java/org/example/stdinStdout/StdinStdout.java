58d83b92717eb8f1a61a43abf51f4241ff03112b
Samuel Dobsa <samuel.dobsa@amcef.sk>
2024-08-27 20:41:52 +0200

    The code above creates a Scanner object named  and uses it to read a String and an int. It then closes the Scanner object because there is no more input to read, and prints to stdout using System.out.println(String).
    So, if our input is:
    Hi 5
    
    Our code will print:
    myString is: Hi
    myInt is: 5
    
    Alternatively, you can use the BufferedReader class.
    
    Task
    In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
    
    Input Format
    
    There are  lines of input, and each line contains a single integer.

---
+++ src/main/java/org/example/stdinStdout/StdinStdout.java
@@ -0,0 +1,20 @@
+package org.example.stdinStdout;
+
+import java.util.Scanner;
+
+public class StdinStdout {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
