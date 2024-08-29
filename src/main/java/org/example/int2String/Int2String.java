0ab16ad197287f39150cf958ac1a5a08ec4119b3
Samuel Dobsa <samuel.dobsa@amcef.sk>
2024-08-30 00:00:10 +0200

    Just int to String.

---
+++ src/main/java/org/example/int2String/Int2String.java
@@ -0,0 +1,12 @@
+package org.example.int2String;
+
+public class Int2String {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int n = 5;
+
+        String s = String.valueOf(n);
+
+    }
+}
